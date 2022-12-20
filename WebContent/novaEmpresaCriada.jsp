39141a49b4f152b77e115f082fd829d37b09e780
Gabriel Sene <sene300@gmail.com>
2022-12-20 20:20:12 -0300

    feat: RequestDisptacher

--- WebContent/novaEmpresaCriada.jsp
+++ WebContent/novaEmpresaCriada.jsp
@@ -1,8 +1,10 @@
 <%
-    String nomeEmpresa = "Alura";
+    
+//scriplet
+	String nomeEmpresa = (String) request.getAttribute("empresa");
     System.out.println(nomeEmpresa);
 %>
 
 <html><body>
-Empresa <% out.println(nomeEmpresa); %>  cadastrada com sucesso!
+Empresa <%= (nomeEmpresa) %>  cadastrada com sucesso!
 </body></html>
--- src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -3,6 +3,7 @@ package br.com.alura.gerenciador.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,21 +17,22 @@ import javax.servlet.http.HttpServletResponse;
 public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Cadastrando nova empresa");
-		PrintWriter out = response.getWriter();
-		
-		
-		//Método para pegar 
-		String nomeEmpresa = request.getParameter("nome");
-		Empresa empresa = new Empresa();
-		empresa.setNome(nomeEmpresa);
-		
-		Banco banco = new Banco();
-		banco.adiciona(empresa);
-		
-		out.println("<html><body>Empresa "+nomeEmpresa +" cadastrada com sucesso "+ "</body></html>");
-		System.out.println("Empresa: "+empresa.getNome()+", Cadastrada com sucesso!");
-	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        System.out.println("Cadastrando nova empresa");
+
+        String nomeEmpresa = request.getParameter("nome");
+        Empresa empresa = new Empresa();
+        empresa.setNome(nomeEmpresa);
+
+        Banco banco = new Banco();
+        banco.adiciona(empresa);
+
+        //chamar o JSP
+        RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+        request.setAttribute("empresa", empresa.getNome());
+        rd.forward(request, response);
+
+    }
 
 }
