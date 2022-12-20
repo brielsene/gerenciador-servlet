f2eb76ef11e84b6e7ffe7bd2f8b055bf1175bba6
Gabriel Sene <sene300@gmail.com>
2022-12-20 20:02:30 -0300

    feat: primeiro JSP

--- src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -6,6 +6,16 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
+	
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setNome("Caelum");
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("Alura");
+		lista.add(empresa);
+		lista.add(empresa2);
+		
+	}
 
 	public void adiciona(Empresa empresa) {
 		lista.add(empresa);
